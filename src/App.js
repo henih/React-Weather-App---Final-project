05734ac76de6ef7781908929478a67d10f3df5a8
henih <86572062+henih@users.noreply.github.com>
2022-01-06 15:48:17 +0100

    Fixed GitHub link

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ export default function App() {
         <a
           href="https://github.com/henih/React-Weather-App---Final-project"
           target="_blank"
+          rel="noreferrer"
         >
           open-sourced on GitHub.
         </a>
