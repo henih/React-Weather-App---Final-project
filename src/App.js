4a50fe93c63c32cb8a1bd1cfabf661501d27cefd
henih <86572062+henih@users.noreply.github.com>
2022-01-06 15:43:39 +0100

    Added the Git repo URL in Footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather Application</div>;
+  return (
+    <div className="App">
+      Weather Application
+      <footer>
+        This application was coded by Henriett Horvath and is{" "}
+        <a
+          href="https://github.com/henih/React-Weather-App---Final-project"
+          target="_blank"
+        >
+          open-sourced on GitHub.
+        </a>
+      </footer>
+    </div>
+  );
 }
