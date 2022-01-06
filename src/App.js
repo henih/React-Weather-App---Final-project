9c5d2fe58f954b03722ba229e54ba1c67c2020fb
henih <86572062+henih@users.noreply.github.com>
2022-01-06 16:16:40 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather Application
-      <footer>
-        This application was coded by Henriett Horvath and is{" "}
-        <a
-          href="https://github.com/henih/React-Weather-App---Final-project"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub.
-        </a>
-      </footer>
+      <div className="container">
+        <h1> Weather Application</h1>
+        <footer>
+          This application was coded by Henriett Horvath and is{" "}
+          <a
+            href="https://github.com/henih/React-Weather-App---Final-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
