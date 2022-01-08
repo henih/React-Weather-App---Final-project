d4449307908edca9c10aa4c6c6637e14eeefaaf9
henih <86572062+henih@users.noreply.github.com>
2022-01-08 14:41:22 +0100

    Added Daily Forecast API call

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,8 +1,19 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import axios from "axios";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+  const apiKey = "c95b7792f2deeca8f4736c654342258c";
+  let longitude = props.coordinates.lon;
+  let latitude = props.coordinates.lat;
+  let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiURL).then(handleResponse);
+
   return (
     <div className="WeatherForecast">
       <div className="row">
